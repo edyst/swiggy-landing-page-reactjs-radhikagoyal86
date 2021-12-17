35ff1337fa85ebee73d81298159235948de7a0ba
yuvikaarora21 <80566379+radhikagoyal86@users.noreply.github.com>
2021-12-17 19:43:51 +0530

    final resubmitted

--- src/Components/UI/Button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Button.css";
-function Button(props) {
-    const classes = "button " + props.className;
-  return <button onClick={props.onClick} className={classes}>{props.children}</button>;
-}
-
-export default Button;
--- src/Components/UI/Transition.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Transition.css";
-const Transition = (props) => {
-  return <div className="transitions">{props.children}</div>;
-};
-
-export default Transition;
